f695f4fc0d58b892991c9238d3d666729058706d
Sour64 <digitalsorrow64@gmail.com>
2025-07-26 22:09:59 +0300

    cors url fix

--- socket/index.js
+++ socket/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        origin: ["https://realtime-chat-app-1-8w8i.onrender.com", "http://localhost:5173"],
         methods: ["GET", "POST"],
     }
 });
@@ -39,6 +39,7 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
     console.log(`Socket.IO server running on port ${PORT}`);
 });
